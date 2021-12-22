39aca5c00b945ccee885f2805788ef2a98476cc8
NayanVR <nvradadiya281@gmail.com>
2021-12-22 21:16:04 +0530

    Quick sort added

--- 13. Sorting/QuickSort.java
+++ 13. Sorting/QuickSort.java
@@ -1,5 +1,43 @@
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] args) {
 
+        int[] arr = { 6, 5, 4, 3, 2, 1 };
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+
+        int s = low;
+        int e = high;
+        int m = s + (e - s) / 2;
+        int pivot = arr[m];
+
+        while (s <= e) {
+            while (arr[s] < pivot) {
+                s++;
+            }
+            while (arr[e] > pivot) {
+                e--;
+            }
+
+            if (s <= e) {
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        // now pivot is at correct index. now solve two halves
+
+        sort(arr, low, e);
+        sort(arr, s, high);
     }
 }
